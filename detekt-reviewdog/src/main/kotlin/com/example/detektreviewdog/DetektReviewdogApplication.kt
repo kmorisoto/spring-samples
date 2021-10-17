272cf40717aa631d549b3c35f664efbec5a041c0
morisoto <k.morisoto@quo-digital.jp>
2021-10-17 16:39:23 +0900

    Initial commit

---
+++ detekt-reviewdog/src/main/kotlin/com/example/detektreviewdog/DetektReviewdogApplication.kt
@@ -0,0 +1,11 @@
+package com.example.detektreviewdog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DetektReviewdogApplication
+
+fun main(args: Array<String>) {
+    runApplication<DetektReviewdogApplication>(*args)
+}
