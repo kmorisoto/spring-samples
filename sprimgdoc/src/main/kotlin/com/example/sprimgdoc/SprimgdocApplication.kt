39ab65b4d37e0ff65dfa799a8b61c46f7d6f9764
morisoto <k.morisoto@quo-digital.jp>
2021-11-19 14:57:27 +0900

    aaa

---
+++ sprimgdoc/src/main/kotlin/com/example/sprimgdoc/SprimgdocApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sprimgdoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprimgdocApplication
+
+fun main(args: Array<String>) {
+    runApplication<SprimgdocApplication>(*args)
+}
---
+++ sprimgdoc/src/test/kotlin/com/example/sprimgdoc/SprimgdocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sprimgdoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprimgdocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
