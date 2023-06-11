ee82e5ab22e55aad1f63c15a5863371aa6b41b43
morisoto <k.morisoto@quo-digital.jp>
2023-06-11 15:16:49 +0900

    add sample

---
+++ datadog/src/test/kotlin/com/example/datadog/DatadogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.datadog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatadogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
