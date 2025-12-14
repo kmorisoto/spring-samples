78aceab0997b580f783b87e31766e8023302b4d8
morisoto <43770663+kmorisoto@users.noreply.github.com>
2025-12-14 16:42:43 +0900

    initial commit

---
+++ boot4/src/main/kotlin/org/example/boot4/Boot4Application.kt
@@ -0,0 +1,11 @@
+package org.example.boot4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Boot4Application
+
+fun main(args: Array<String>) {
+    runApplication<Boot4Application>(*args)
+}
---
+++ boot4/src/test/kotlin/org/example/boot4/Boot4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.boot4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Boot4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
